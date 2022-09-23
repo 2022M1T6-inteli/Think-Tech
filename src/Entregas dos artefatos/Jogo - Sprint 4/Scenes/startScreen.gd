79f83f22f2ac2db6bf2726a6ba7830fadf3d9340
Celineps <110488902+Celinepsouza@users.noreply.github.com>
2022-09-23 14:02:18 -0300

    sprint 4

--- src/Entregas dos artefatos/Jogo - Sprint 4/Scenes/startScreen.gd
+++ src/Entregas dos artefatos/Jogo - Sprint 4/Scenes/startScreen.gd
@@ -16,18 +16,18 @@ func _process(delta):
 		$quitBtn.text="Sair"
 		$langBtn.text="PT-BR"
 func _ready():
-	if !MusicGlobal.is_playing: 
+	if !MusicGlobal.is_playing: #Inicia a música
 		MusicGlobal.play_music()
 
 #  Inicia o jogo.	
 func _on_startBtn_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Video.tscn")
-	MusicGlobal.stop_music()
+	MusicGlobal.stop_music() # Para a música
 	
 
 
-# Mostra os controles do jogo.	
+# Abre a tela de opções do jogo	
 func _on_controlBtn_pressed() -> void:
 # warning-ignore:return_value_discarded
 	get_tree().change_scene("res://Scenes/Control.tscn")
@@ -38,11 +38,11 @@ func _on_controlBtn_pressed() -> void:
 func _on_quitBtn_pressed() -> void:
 	get_tree().quit()
 
-
+# Abre a tela da constelação e mostra todos os planetas
 func _on_Mapas_pressed():
 	get_tree().change_scene("res://Scenes/TeladosMapas.tscn")
 
-
+# Troca o idioma
 func _on_langBtn_pressed():
 	if Global.lang==1:
 		Global.lang=2
